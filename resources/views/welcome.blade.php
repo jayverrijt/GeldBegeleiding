d997bfd3fdde91e4f8230bd9bc361edcdf213f20
Jayze Verrijt <109523337+jayverrijt@users.noreply.github.com>
2024-07-09 15:13:07 +0200

    [9-7-24] Header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
         <link rel="icon" type="image/png" sizes="200x200" rel="noopener" target="_blank" href="{{asset('store/pictures/logos/logo-200.png')}}">
         <link rel="icon" type="image/png" sizes="500x500" rel="noopener" target="_blank" href="{{asset('store/pictures/logos/logo-500.png')}}">
         <link rel="icon" type="image/png" sizes="1000x1000" rel="noopener" target="_blank" href="{{asset('store/pictures/logo/logo-1000.png')}}">
+        <link rel="icon" type="image/png" rel="noopener" target="_blank" href="{{asset('store/pictures/logos/logo-500.png')}}">
+
 
 
         <title>{{$title}}</title>
